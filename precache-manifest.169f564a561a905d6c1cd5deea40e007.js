5a527e9679f41e9c09ba772eb79e480ef2dd471e
Traian Stanciu <stanciu.traian@gmail.com>
2019-09-08 18:56:48 +0300

    Updates

---
+++ precache-manifest.169f564a561a905d6c1cd5deea40e007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1756d65ada4180f221a8dcec3efeb4ae",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "4aba0bec8b0d9073aa62",
+    "url": "/react-cv/static/css/main.24fd6f7c.chunk.css"
+  },
+  {
+    "revision": "e435fc894189dea24d7b",
+    "url": "/react-cv/static/js/2.14496b2f.chunk.js"
+  },
+  {
+    "revision": "4aba0bec8b0d9073aa62",
+    "url": "/react-cv/static/js/main.f119ef35.chunk.js"
+  },
+  {
+    "revision": "5836cec8f528c461dc43",
+    "url": "/react-cv/static/js/runtime~main.a6f26e18.js"
+  }
+]);
